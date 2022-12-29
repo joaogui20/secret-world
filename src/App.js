81e9fdd98eb24945b3e56f32e8612867e16e90db
João Guilherme <azazazazaisjoao@gmail.com>
2022-12-29 16:29:12 -0400

    Finalizando tela de fim de jogo

--- src/App.js
+++ src/App.js
@@ -130,7 +130,7 @@ function App() {
           score={score}
         />
       )}
-      {gameStage === 'end' && <GameOver retry={retry} />}
+      {gameStage === 'end' && <GameOver retry={retry} score={score} />}
     </div>
   );
 }
