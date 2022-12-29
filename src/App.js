a687de2c87beea2d51bc904882f201fbbd3bca51
João Guilherme <azazazazaisjoao@gmail.com>
2022-12-29 16:14:51 -0400

    Processando letras válidas e inválidas

--- src/App.js
+++ src/App.js
@@ -69,8 +69,29 @@ function App() {
 
   // Process the letter input
   const verifyLetter = (letter) => {
-    console.log(letter);
+    const normalizedLetter = letter.toLowerCase();
+
+    // Check if letter has already been utilized
+    if (guessedLetters.includes(normalizedLetter) || wrongLetters.includes(normalizedLetter)) {
+      return;
+    }
+
+    // Push guessed letter or remove a guess
+    if (letters.includes(normalizedLetter)) {
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters,
+        normalizedLetter,
+      ]);
+    } else {
+      setWrongLetters((actualWrongLetters) => [
+        ...actualWrongLetters,
+        normalizedLetter,
+      ]);
+    }
   };
+
+  console.log(guessedLetters);
+  console.log(wrongLetters);
 
   // Restarts the game
   const retry = () => {
